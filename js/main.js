64c7008bc46849ba5a972a97762d9e5ea03a38da
giuliasb296 <giuliasb296@gmail.com>
2023-08-04 16:14:38 -0300

    Update

--- js/main.js
+++ js/main.js
@@ -3,3 +3,12 @@ document.getElementById("menuIcon").addEventListener("click", function(event) {
     menuContent.style.display = menuContent.style.display === 'block' ? 'none' : 'block'
     event.stopPropagation;
 })
+
+document.addEventListener("click", function(event){
+    let menuContent = document.getElementById('menuContent');
+    let menuIcon = document.getElementById("menuIcon");
+    if(!menuIcon.contains(event.target) && !menuContent.contains(event.target)) {
+        menuContent.style.display = 'none'
+    }
+    
+})
